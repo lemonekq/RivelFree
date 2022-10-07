1d8c2e0e3050959cf8de0195a45c73abb38d23fe
lemonek <72318911+lemonekq@users.noreply.github.com>
2022-10-07 22:02:04 +0200

    yah

--- src/RivelFree/home.xaml.cs
+++ src/RivelFree/home.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Management;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Markup;
 
 namespace RivelFree
 {
@@ -9,6 +11,10 @@ namespace RivelFree
     /// </summary>
     public partial class home : Window
     {
+        // variables
+        private string cpuname, cpucores;
+        private float ramfloat;
+
         public home()
         {
             InitializeComponent();
@@ -23,6 +29,8 @@ namespace RivelFree
             Style s = new Style();
             s.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
             tabcontrolcontainer.ItemContainerStyle = s;
+
+            getpcinfo();
         }
 
         private void dragbox_MouseDown(object sender, MouseButtonEventArgs e)
@@ -38,5 +46,68 @@ namespace RivelFree
             // ez
             this.Close();
         }
+
+        // for hardware page
+        void getpcinfo()
+        {
+            // get cpu name
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                cpuname = (string)mo["Name"];
+            }
+
+            // get cores
+            cpucores = Environment.ProcessorCount.ToString();
+
+            cpulabel.Content = cpuname + " / " + cpucores + "cores";
+
+            // get gpu
+            using (var searcher = new ManagementObjectSearcher("select * from Win32_VideoController"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    gpulabel.Content = obj["Name"];
+                }
+            }
+
+            // get ram
+            ManagementObjectSearcher Search = new ManagementObjectSearcher("Select * From Win32_ComputerSystem");
+
+            foreach (ManagementObject Mobject in Search.Get())
+            {
+                double Ram_Bytes = (Convert.ToDouble(Mobject["TotalPhysicalMemory"]));
+
+                // it kinda works. kinda
+                ConnectionOptions connection = new ConnectionOptions();
+                connection.Impersonation = ImpersonationLevel.Impersonate;
+                ManagementScope scope = new ManagementScope("\\\\.\\root\\CIMV2", connection);
+                scope.Connect();
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory"); ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query); foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    // cut the float, this isnt the pi number LOL
+                    ramfloat = (float)(Ram_Bytes / 1073741824);
+                    ramfloat = (float)System.Math.Round(ramfloat, 2);
+                    ramlabel.Content = (string)ramfloat.ToString() + "GB / " + GetMemoryType(Int32.Parse(queryObj["MemoryType"].ToString()));
+                }
+            }
+        }
+
+        // i will update it later for ddr5
+        public string GetMemoryType(int MemoryType)
+        {
+            switch (MemoryType)
+            {
+                case 20:
+                    return "DDR-2";
+                case 21:
+                    return "DDR-3";
+                default:
+                    if (MemoryType == 0 || MemoryType > 22)
+                        return "DDR-4-5";
+                    else
+                        return "Other";
+            }
+        } 
     }
 }
