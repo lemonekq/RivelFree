f192de7c8a09a6d267a8e08623c6585e35d0bdaf
lemonek <72318911+lemonekq@users.noreply.github.com>
2022-10-13 21:20:45 -0700

    clickable icons

--- src/RivelFree/home.xaml.cs
+++ src/RivelFree/home.xaml.cs
@@ -214,6 +214,38 @@ namespace RivelFree
             Process.Start("net.exe", "start UsoSvc");
         }
 
+        private void hardwareicon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(1);
+        }
+
+        private void networkicon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(2);
+        }
+
+        private void latencyicon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(3);
+        }
+
+        private void cleanericon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(4);
+        }
+
+        private void systemicon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(5);
+        }
+
+        private void tweaksicon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            switchwindow(6);
+        }
+
+        private void optimizationicon_MouseDown(object sender, MouseButtonEventArgs e) { switchwindow(7); }
+
         // if someone has ddr5 for testing, dm me!
         public string GetMemoryType(int MemoryType)
         {
