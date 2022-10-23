9d8b1c568bfe988dd67c640fb9d4fcb8e057e102
lemonek <72318911+lemonekq@users.noreply.github.com>
2022-10-23 21:45:27 +0200

    dl-urls (2/3)

--- src/RivelFree/download-urls.cs
+++ src/RivelFree/download-urls.cs
@@ -4,10 +4,9 @@
     {
         // easy update download links
         public static string update_url = "https://raw.githubusercontent.com/lemonekq/RivelFree/main/res/static.txt"; // get update info
-        public static string update_url_filename = "static.txt";
 
         public static string cpu_pow = "https://raw.githubusercontent.com/lemonekq/RivelFree/main/reg/performance/cpu.pow"; // power plan
-        public static string cpu_pow_filename = "cpu.pow";
+        public static string cpu_pow_filename = cpu_pow.Substring(cpu_pow.LastIndexOf('-') + 1);
 
     }
 }
